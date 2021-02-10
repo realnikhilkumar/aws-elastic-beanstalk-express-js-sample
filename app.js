574141f7f8a1f3088c67e6c8a512bd92fafeb2ca
realnikhilkumar <69275062+realnikhilkumar@users.noreply.github.com>
2021-02-10 13:49:02 -0500

    Full Pipeline Test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is AWS CICD Tutorial!'));
+app.get('/', (req, res) => res.send('This is AWS CICD Tutorial. Putting a new release to review pipeline stages!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
